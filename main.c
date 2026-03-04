4f4c5be142c2245f311cd4d30001da5a88b5a98a
kuri1o <tomas.kurial2009@gmail.com>
2026-03-04 10:22:25 +0100

    kakam

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include<windows.h>
 /* KURIHO CASINO (a kocyho) */
 //https://code-with-me.global.jetbrains.com/2Io03S_JenD7w6Pie1cETQ
+//curam
+//kakam
 void OBCHOD(int *penize,int *TOKEN) {
     int menuo,nakupovat=1;
 while (nakupovat == 1) {
